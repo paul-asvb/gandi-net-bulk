763e08ca58688bb0b589e8242f47225e0f4ae618
Paul <paul@paul-mayer.eu>
2021-06-07 11:44:55 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# bulk query gandi net domains
+
+## prerequisities:
+```
+npm install
+```
+
+apikey from gandi.net in .env file
+
+## update available tdls
+Update the Top Level domain file (available-tdls.json)
+```
+npm run update-tlds
+```
+
+## run domain query
---
+++ update_tlds.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+const fs = require('fs')
+require('dotenv').config()
+
+const GANDI_KEY = process.env.GANDI_KEY;
+const tdl_file = './available-tdls.json'
+const headers= {
+    'Content-Type': 'application/json',
+    'authorization':`Apikey ${GANDI_KEY}`
+    }
+
+fetch(' https://api.gandi.net/v5/domain/tlds', {
+        method: 'get',
+        headers,
+    })
+    .then(res => res.json())
+    .then((json)=>{
+
+        fs.writeFile(tdl_file, JSON.stringify(json), function (err,data) {
+            if (err) {
+              return console.log(err);
+            }else{
+
+            console.log(`${tdl_file} updated.`);
+                            }
+          });
+    });
